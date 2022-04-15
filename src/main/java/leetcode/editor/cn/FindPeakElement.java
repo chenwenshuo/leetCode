9ef71d6c77f26e27e9fd2364613cb2a1fbcaa9fa
chenwenshuo <chenwenshuo@58.com>
2022-04-15 10:27:09 +0800

    82. 删除排序链表中的重复元素 II

---
+++ src/main/java/leetcode/editor/cn/FindPeakElement.java
@@ -0,0 +1,71 @@
+package leetcode.editor.cn;
+
+//峰值元素是指其值严格大于左右相邻值的元素。 
+//
+// 给你一个整数数组 nums，找到峰值元素并返回其索引。数组可能包含多个峰值，在这种情况下，返回 任何一个峰值 所在位置即可。 
+//
+// 你可以假设 nums[-1] = nums[n] = -∞ 。 
+//
+// 你必须实现时间复杂度为 O(log n) 的算法来解决此问题。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：nums = [1,2,3,1]
+//输出：2
+//解释：3 是峰值元素，你的函数应该返回其索引 2。 
+//
+// 示例 2： 
+//
+// 
+//输入：nums = [1,2,1,3,5,6,4]
+//输出：1 或 5 
+//解释：你的函数可以返回索引 1，其峰值元素为 2；
+//     或者返回索引 5， 其峰值元素为 6。
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= nums.length <= 1000 
+// -2³¹ <= nums[i] <= 2³¹ - 1 
+// 对于所有有效的 i 都有 nums[i] != nums[i + 1] 
+// 
+// Related Topics 数组 二分查找 👍 780 👎 0
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class FindPeakElement{
+         public static void main(String[] args) {
+             Solution solution = new FindPeakElement().new Solution();
+             System.out.println(solution.findPeakElement(new int[]{1, 2, 1, 3, 5, 6, 4}));
+         }
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int findPeakElement(int[] nums) {
+
+        if (nums.length==1) return nums[0];
+        int l=1;
+        int r=nums.length-2;
+        while (l<=r){
+            int mid=l+(r-l)/2;
+            if(nums[mid]>nums[mid+1]&&nums[mid]>nums[mid-1])
+                return mid;
+            l=mid+1;
+
+        }
+
+        return 0;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
---
+++ src/main/java/leetcode/editor/cn/RemoveDuplicatesFromSortedListIi.java
@@ -0,0 +1,79 @@
+package leetcode.editor.cn;
+
+//给定一个已排序的链表的头 head ， 删除原始链表中所有重复数字的节点，只留下不同的数字 。返回 已排序的链表 。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：head = [1,2,3,3,4,4,5]
+//输出：[1,2,5]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：head = [1,1,1,2,3]
+//输出：[2,3]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 链表中节点数目在范围 [0, 300] 内 
+// -100 <= Node.val <= 100 
+// 题目数据保证链表已经按升序 排列 
+// 
+// Related Topics 链表 双指针 👍 863 👎 0
+
+import java.util.List;
+
+public class RemoveDuplicatesFromSortedListIi{
+         public static void main(String[] args) {
+            Solution solution = new RemoveDuplicatesFromSortedListIi().new Solution();
+ }
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+  //Definition for singly-linked list.
+  public class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode listNode = new ListNode(0, head);
+        ListNode ans=listNode;
+        /**
+         *0--> 1->2>2>2>3>3>4
+         * ans  1->2>2>2>3>3>4
+         *     1>2>2>3>3>4
+         *      1>2>3>3>4
+         *      1>3>3>4
+         *      1>3>4
+         *      1>4
+         *
+         */
+        while (ans.next!=null&&ans.next.next!=null){
+            if (ans.next.val==ans.next.next.val){
+                int x=ans.next.val;
+                while (ans.next!=null&&ans.next.val==x){
+                    ans.next=ans.next.next;
+                }
+            }else {
+                ans=ans.next;
+            }
+
+        }
+        return listNode.next;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
