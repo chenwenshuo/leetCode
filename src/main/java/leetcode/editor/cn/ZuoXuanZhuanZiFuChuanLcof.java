9038869f82a8e880abf969e6ef65fab9c94f7834
chenwenshuo <chenwenshuo@58.com>
2022-06-23 17:41:59 +0800

    剑指 Offer 58 - II. 左旋转字符串

---
+++ src/main/java/leetcode/editor/cn/ZuoXuanZhuanZiFuChuanLcof.java
@@ -0,0 +1,46 @@
+package leetcode.editor.cn;
+
+//字符串的左旋转操作是把字符串前面的若干个字符转移到字符串的尾部。请定义一个函数实现字符串左旋转操作的功能。比如，输入字符串"abcdefg"和数字2，该函数
+//将返回左旋转两位得到的结果"cdefgab"。 
+//
+// 
+//
+// 示例 1： 
+//
+// 输入: s = "abcdefg", k = 2
+//输出: "cdefgab"
+// 
+//
+// 示例 2： 
+//
+// 输入: s = "lrloseumgh", k = 6
+//输出: "umghlrlose"
+// 
+//
+// 
+//
+// 限制： 
+//
+// 
+// 1 <= k < s.length <= 10000 
+// 
+// Related Topics 数学 双指针 字符串 👍 263 👎 0
+
+public class ZuoXuanZhuanZiFuChuanLcof{
+         public static void main(String[] args) {
+            Solution solution = new ZuoXuanZhuanZiFuChuanLcof().new Solution();
+ }
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public String reverseLeftWords(String s, int n) {
+        StringBuilder res = new StringBuilder();
+        for(int i = n; i < n + s.length(); i++)
+            res.append(s.charAt(i % s.length()));
+        return res.toString();
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
