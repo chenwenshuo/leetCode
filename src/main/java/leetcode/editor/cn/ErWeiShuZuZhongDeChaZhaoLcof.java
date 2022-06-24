2848354962ff355a3e47455fda4b766803eb8156
chenwenshuo <chenwenshuo@58.com>
2022-06-24 15:29:32 +0800

    剑指 Offer 04. 二维数组中的查找  左上角

---
+++ src/main/java/leetcode/editor/cn/ErWeiShuZuZhongDeChaZhaoLcof.java
@@ -0,0 +1,73 @@
+package leetcode.editor.cn;
+
+//在一个 n * m 的二维数组中，每一行都按照从左到右递增的顺序排序，每一列都按照从上到下递增的顺序排序。请完成一个高效的函数，输入这样的一个二维数组和一个
+//整数，判断数组中是否含有该整数。 
+//
+// 
+//
+// 示例: 
+//
+// 现有矩阵 matrix 如下： 
+//
+// 
+//[
+//  [1,   4,  7, 11, 15],
+//  [2,   5,  8, 12, 19],
+//  [3,   6,  9, 16, 22],
+//  [10, 13, 14, 17, 24],
+//  [18, 21, 23, 26, 30]
+//]
+// 
+//
+// 给定 target = 5，返回 true。 
+//
+// 给定 target = 20，返回 false。 
+//
+// 
+//
+// 限制： 
+//
+// 0 <= n <= 1000 
+//
+// 0 <= m <= 1000 
+//
+// 
+//
+// 注意：本题与主站 240 题相同：https://leetcode-cn.com/problems/search-a-2d-matrix-ii/ 
+// Related Topics 数组 二分查找 分治 矩阵 👍 702 👎 0
+
+public class ErWeiShuZuZhongDeChaZhaoLcof{
+         public static void main(String[] args) {
+            Solution solution = new ErWeiShuZuZhongDeChaZhaoLcof().new Solution();
+ }
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public boolean findNumberIn2DArray(int[][] matrix, int target) {
+
+        if (matrix==null||matrix.length==0) return false;
+        int a=matrix.length-1;
+        int l=0;
+        int b=matrix[0].length-1;
+
+        while (l<=a&&b>=0){
+            while (matrix[l][b]==target){
+                return true;
+            }
+            while (l<=a&&matrix[l][b]<target){
+                l++;
+            }
+            while (b>=0&&l<=a&&matrix[l][b]>target) {
+                b--;
+            }
+
+
+        }
+
+
+        return false;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
