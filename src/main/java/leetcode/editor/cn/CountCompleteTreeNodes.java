24d859607ad16882d38f95ffd0a75b6062eeae21
chenwenshuo <chenwenshuo@58.com>
2022-07-14 15:26:41 +0800

    222. 完全二叉树的节点个数  二分+二进制

---
+++ src/main/java/leetcode/editor/cn/CountCompleteTreeNodes.java
@@ -0,0 +1,106 @@
+package leetcode.editor.cn;
+
+//给你一棵 完全二叉树 的根节点 root ，求出该树的节点个数。 
+//
+// 完全二叉树 的定义如下：在完全二叉树中，除了最底层节点可能没填满外，其余每层节点数都达到最大值，并且最下面一层的节点都集中在该层最左边的若干位置。若最底层
+//为第 h 层，则该层包含 1~ 2ʰ 个节点。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：root = [1,2,3,4,5,6]
+//输出：6
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：root = []
+//输出：0
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：root = [1]
+//输出：1
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树中节点的数目范围是[0, 5  10⁴] 
+// 0 <= Node.val <= 5  10⁴ 
+// 题目数据保证输入的树是 完全二叉树 
+// 
+//
+// 
+//
+// 进阶：遍历树来统计节点是一种时间复杂度为 O(n) 的简单解决方案。你可以设计一个更快的算法吗？ 
+// Related Topics 树 深度优先搜索 二分查找 二叉树 👍 734 👎 0
+
+public class CountCompleteTreeNodes{
+         public static void main(String[] args) {
+            Solution solution = new CountCompleteTreeNodes().new Solution();
+ }
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+  //Definition for a binary tree node.
+  public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+    class Solution {
+        public int countNodes(TreeNode root) {
+            if (root == null) {
+                return 0;
+            }
+            int level = 0;
+            TreeNode node = root;
+            while (node.left != null) {
+                level++;
+                node = node.left;
+            }
+            int low = 1 << level, high = (1 << (level + 1)) - 1;
+            while (low < high) {
+                int mid = (high - low + 1) / 2 + low;
+                if (exists(root, level, mid)) {
+                    low = mid;
+                } else {
+                    high = mid - 1;
+                }
+            }
+            return low;
+        }
+
+        public boolean exists(TreeNode root, int level, int k) {
+            int bits = 1 << (level - 1);
+            TreeNode node = root;
+            while (node != null && bits > 0) {
+                if ((bits & k) == 0) {
+                    node = node.left;
+                } else {
+                    node = node.right;
+                }
+                bits >>= 1;
+            }
+            return node != null;
+        }
+    }
+
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
