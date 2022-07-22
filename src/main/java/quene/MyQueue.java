7ba41a0ec683121ca38ea21f5686467897cd6615
chenwenshuo <chenwenshuo@58.com>
2022-07-22 10:51:22 +0800

    实现队列

---
+++ src/main/java/quene/MyQueue.java
@@ -0,0 +1,59 @@
+package quene;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author: chenwenshuo
+ * @Date: 2022/07/21/3:48 下午
+ */
+public class MyQueue {
+    // store elements
+    private List<Integer> data;
+    // a pointer to indicate the start position
+    private int p_start;
+    public MyQueue() {
+        data = new ArrayList<Integer>();
+        p_start = 0;
+    }
+    /** Insert an element into the queue. Return true if the operation is successful. */
+    public boolean enQueue(int x) {
+        data.add(x);
+        return true;
+    };
+    /** Delete an element from the queue. Return true if the operation is successful. */
+    public boolean deQueue() {
+        if (isEmpty() == true) {
+            return false;
+        }
+        p_start++;
+        return true;
+    }
+    /** Get the front item from the queue. */
+    public int Front() {
+        return data.get(p_start);
+    }
+    /** Checks whether the queue is empty or not. */
+    public boolean isEmpty() {
+        return p_start >= data.size();
+    }
+};
+
+ class Main {
+    public static void main(String[] args) {
+        MyQueue q = new MyQueue();
+        q.enQueue(5);
+        q.enQueue(3);
+        if (q.isEmpty() == false) {
+            System.out.println(q.Front());
+        }
+        q.deQueue();
+        if (q.isEmpty() == false) {
+            System.out.println(q.Front());
+        }
+        q.deQueue();
+        if (q.isEmpty() == false) {
+            System.out.println(q.Front());
+        }
+    }
+}
