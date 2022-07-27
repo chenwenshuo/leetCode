b138b1d758ef19f90aee5f23ac5599d150489acc
chenwenshuo <chenwenshuo@58.com>
2022-07-27 10:36:49 +0800

    121 买股票的最佳时期

--- src/main/java/leetcode/editor/cn/BestTimeToBuyAndSellStock.java
+++ src/main/java/leetcode/editor/cn/BestTimeToBuyAndSellStock.java
@@ -45,14 +45,13 @@ public class BestTimeToBuyAndSellStock {
     //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int maxProfit(int[] prices) {
-        int min=prices[0];
-        int ans=0;
+        //[7,1,5,3,6,4]
+      int min=prices[0];
+      int ans=0;
         for (int i = 1; i < prices.length; i++) {
-
             min=Math.min(min,prices[i]);
             ans=Math.max(ans,prices[i]-min);
         }
-
         return ans;
     }
 }
