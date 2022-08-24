8c0e56b4515e4a63c63f3cc8ea89d6a07c214fe7
chenwenshuo <chenwenshuo@58.com>
2022-08-24 19:54:26 +0800

    9. 回文数

---
+++ src/main/java/leetcode/editor/cn/PalindromeNumber.java
@@ -0,0 +1,73 @@
+package leetcode.editor.cn;
+
+//给你一个整数 x ，如果 x 是一个回文整数，返回 true ；否则，返回 false 。 
+//
+// 回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。 
+//
+// 
+// 例如，121 是回文，而 123 不是。 
+// 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：x = 121
+//输出：true
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：x = -121
+//输出：false
+//解释：从左向右读, 为 -121 。 从右向左读, 为 121- 。因此它不是一个回文数。
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：x = 10
+//输出：false
+//解释：从右向左读, 为 01 。因此它不是一个回文数。
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// -2³¹ <= x <= 2³¹ - 1 
+// 
+//
+// 
+//
+// 进阶：你能不将整数转为字符串来解决这个问题吗？ 
+//
+// Related Topics 数学 👍 2150 👎 0
+
+public class PalindromeNumber{
+         public static void main(String[] args) {
+            Solution solution = new PalindromeNumber().new Solution();
+ }
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public boolean isPalindrome(int x) {
+        //思考：这里大家可以思考一下，为什么末尾为 0 就可以直接返回 false
+        if (x < 0 || (x % 10 == 0 && x != 0)) return false;
+        int revertedNumber = 0;
+        while (x > revertedNumber) {
+            revertedNumber = revertedNumber * 10 + x % 10;
+            x /= 10;
+        }
+        return x == revertedNumber || x == revertedNumber / 10;
+
+
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
