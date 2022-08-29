0cc57a3a2a2a1c4eb8c2cdf9cf3da004eb8d6fd7
chenwenshuo <chenwenshuo@58.com>
2022-08-29 20:01:30 +0800

    1470 重新排列数组

---
+++ src/main/java/leetcode/editor/cn/ShuffleTheArray.java
@@ -0,0 +1,60 @@
+package leetcode.editor.cn;
+
+//给你一个数组 nums ，数组中有 2n 个元素，按 [x1,x2,...,xn,y1,y2,...,yn] 的格式排列。 
+//
+// 请你将数组按 [x1,y1,x2,y2,...,xn,yn] 格式重新排列，返回重排后的数组。 
+//
+// 
+//
+// 示例 1：      2,3,5,4,1,7
+//              0,1,2,3,4,5
+// 输入：nums = [2,5,1,3,4,7], n = 3
+//输出：[2,3,5,4,1,7] 
+//解释：由于 x1=2, x2=5, x3=1, y1=3, y2=4, y3=7 ，所以答案为 [2,3,5,4,1,7]
+// 
+//
+// 示例 2： 
+//
+// 输入：nums = [1,2,3,4,4,3,2,1], n = 4
+//输出：[1,4,2,3,3,2,4,1]
+// 
+//
+// 示例 3： 
+//
+// 输入：nums = [1,1,2,2], n = 2
+//输出：[1,2,1,2]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= n <= 500 
+// nums.length == 2n 
+// 1 <= nums[i] <= 10^3 
+// 
+//
+// Related Topics 数组 👍 129 👎 0
+
+public class ShuffleTheArray{
+         public static void main(String[] args) {
+            Solution solution = new ShuffleTheArray().new Solution();
+             System.out.println(solution.shuffle(new int[]{2,5,1,3,4,7},3));
+ }
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+        int[] ans=new int[2*n];
+        ans[0]=nums[0];
+        for (int i = 0; i < n; i++) {
+            ans[2*i]=nums[i];
+            ans[2*(i+1)-1]=nums[n+i];
+        }
+        return ans;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
