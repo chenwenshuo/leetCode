69b3afdb9dba76cb0b15eb012d6487c45887b1a4
chenwenshuo <chenwenshuo@58.com>
2022-09-01 17:48:36 +0800

    119 杨辉三角Ⅱ

---
+++ src/main/java/leetcode/editor/cn/PascalsTriangleIi.java
@@ -0,0 +1,70 @@
+package leetcode.editor.cn;
+
+//给定一个非负索引 rowIndex，返回「杨辉三角」的第 rowIndex 行。 
+//
+// 在「杨辉三角」中，每个数是它左上方和右上方的数的和。 
+//
+// 
+//
+// 
+//
+// 示例 1: 
+//
+// 
+//输入: rowIndex = 3
+//输出: [1,3,3,1]
+// 
+//
+// 示例 2: 
+//
+// 
+//输入: rowIndex = 0
+//输出: [1]
+// 
+//
+// 示例 3: 
+//
+// 
+//输入: rowIndex = 1
+//输出: [1,1]
+// 
+//
+// 
+//
+// 提示: 
+//
+// 
+// 0 <= rowIndex <= 33 
+// 
+//
+// 
+//
+// 进阶： 
+//
+// 你可以优化你的算法到 O(rowIndex) 空间复杂度吗？ 
+//
+// Related Topics 数组 动态规划 👍 425 👎 0
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PascalsTriangleIi{
+         public static void main(String[] args) {
+            Solution solution = new PascalsTriangleIi().new Solution();
+ }
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> row = new ArrayList<Integer>();
+        row.add(1);
+        for (int i = 1; i <= rowIndex; ++i) {
+            row.add((int) ((long) row.get(i - 1) * (rowIndex - i + 1) / i));
+        }
+        return row;
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
