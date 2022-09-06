ee3df8855c97c80318af8c246e31b8f9b9e19b64
chenwenshuo <chenwenshuo@58.com>
2022-09-06 17:22:25 +0800

    828. 统计子串中的唯一字符   求每个贡献值

--- src/main/java/leetcode/editor/cn/CountUniqueCharactersOfAllSubstringsOfAGivenString.java
+++ src/main/java/leetcode/editor/cn/CountUniqueCharactersOfAllSubstringsOfAGivenString.java
@@ -60,7 +60,20 @@ public class CountUniqueCharactersOfAllSubstringsOfAGivenString{
  }
 
 //leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
+
+    /**
+     * 原问题为求所有子数组的唯一字符数量和，其可等价为求每个 s[i]s[i] 对答案的贡献，即每个 s[i]s[i] 可作为多少个子数组的唯一元素。
+     *
+     * 假定我们能预处理出两数组 l 和 r 分别代表 s[i]s[i] 作为子数组唯一字符时，其所能到达的最远两端：
+     *
+     * l[i] = a 代表下标 aa 为 s[i]s[i] 能够作为子数组唯一字符时的最远左边界，即为 s[i]s[i] 左边第一个与 s[i]s[i] 值相同的位置（若不存在，则为 a = -1a=−1）
+     * r[i] = b 代表跳表 bb 为 s[i]s[i] 能够作为子数组唯一字符时的最远右边界，即为 s[i]s[i] 右边第一个与 s[i]s[i] 值相同的位置（若不存在，则为 b = nb=n）
+     * 子数组左端点个数为 (i - a)(i−a) 个，右端点个数为 (b - i)(b−i) 个，根据乘法原理可知，子数组个数为两者乘积。
+     *
+     * 预处理 l 和 r 只需要使用遍历计数即可
+     *
+     */
+    class Solution {
     public int uniqueLetterString(String s) {
         char[] cs = s.toCharArray();
         int n = cs.length, ans = 0;
