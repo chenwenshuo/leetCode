ec76b48fbcbd7265e39b753c8d6afd5bd96de8fc
chenwenshuo <chenwenshuo@58.com>
2022-09-22 20:06:19 +0800

    hot100 5-6

--- src/main/java/hot100/hot5.java
+++ src/main/java/hot100/hot5.java
@@ -19,9 +19,44 @@ package hot100;
  * @Date: 2022/09/22/19:12
  */
 public class hot5 {
+    public static void main(String[] args) {
+        hot5 hot5 = new hot5();
+        String s = hot5.longestPalindrome("bb");
+        System.out.println(s);
+    }
     public String longestPalindrome(String s) {
+        char[] chars = s.toCharArray();
+        int length = chars.length;
+        boolean[][] dp=new boolean[length][length];
+        for (int i = 0; i < chars.length; i++) {
+            dp[i][i]=true;
+        }
+        int len=1;
+        int begin=0;
+        for (int L = 2; L <=length; L++) {
+            for (int i = 0; i < chars.length; i++) {
+                int j=i+L-1;
+                if(j>=chars.length) break;
+                if (chars[i]!=chars[j]){
+                    dp[i][j]=false;
+                }else {
+                    if (j-i<3){
+                        dp[i][j]=true;
+                    }else {
+                        dp[i][j]=dp[i+1][j-1];
+                    }
+                }
 
-        return null;
+                if (dp[i][j]&&j-i+1>len){
+                    begin=i;
+                    len=j-i+1;
+                }
+
+            }
+
+        }
+
+        return s.substring(begin,begin+len);
 
     }
 }
